54551014c9276129ecb8fbdadec0ccda262761e5
ThanieaLuqizahwa <tluqizahwa@gmail.com>
2024-11-18 22:03:38 +0700

    first commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,25 @@
+const getAllProductCategories = ()  => {
+  return [   {
+    id: 'PNSL',
+    slug: 'pensil',
+    name: `Pensil`
+  },
+  {
+    id: 'BNDR',
+    slug: 'binder',
+    name: `Binder`
+  },
+  {
+    id: 'KLIP',
+    slug: 'klip',
+    name: `Klip`
+  },
+  {
+    id: 'LAIN',
+    slug: 'item-lain',
+    name: `Item Lainnya`
+  },
+  ]
+}
+
+export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,180 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'stl-il-LAIN-001',
+      slug: 'stabilo-item-lain-il',
+      name: "Stabilo Boss",
+      category: `NEW ITEM`,
+      categorySlug: 'item-lain',
+      price: 8_500,
+      stock: 100,
+      imageUrl: '/assets/images/stabilo.jpg',
+      description: `Stabilo Boss adalah highlighter yang sangat praktis dan berguna untuk berbagai keperluan. Desainnya yang ikonik, kualitas tintanya yang baik, dan pilihan warnanya yang beragam membuat Stabilo Boss menjadi pilihan favorit banyak orang.`
+    },
+    {
+      id: 'pnsl-wrn-PNSL-001',
+      slug: 'pensil-warna-pnsl-pan',
+      name: "Pensil Warna",
+      category: `BEST BUY`,
+      categorySlug: 'pensil',
+      price: 2_500,
+      stock: 50,
+      imageUrl: `/assets/images/pensil warna warni.jpeg`,
+      description: `Pensil warna adalah alat yang sangat serbaguna dan menyenangkan untuk digunakan. Baik Anda seorang seniman profesional atau hanya ingin mengisi waktu luang, pensil warna dapat membantu Anda mengeksplorasi kreativitas Anda.`
+    },
+    {
+      id: 'plpn-wrn-PNSL-001',
+      slug: 'pulpen-warna-pnsl-pan',
+      name: "Pulpen Warna-Warni",
+      category: `BEST SALLER`,
+      categorySlug: 'pensil',
+      price: 3_000,
+      stock: 40,
+      imageUrl: `/assets/images/pulpen warna warni.webp`,
+      description: `Pulpen warna adalah alat tulis yang serbaguna dan menyenangkan untuk digunakan. Baik untuk keperluan sekolah, kantor, atau hobi, pulpen warna dapat memberikan sentuhan warna pada karya-karya Anda.`
+    },
+    {
+      id: 'pnsl-mknk-PNSL-001',
+      slug: 'pensil-mekanik-pnsl-pan',
+      name: "Pensil Mekanik",
+      category: `BEST BUY`,
+      categorySlug: 'pensil',
+      price: 4_000,
+      stock: 45,
+      imageUrl: `/assets/images/pensil mekanik.jpg`,
+      description: `Pensil mekanik adalah alat tulis yang sangat praktis dan efisien. Jika Anda mencari alat tulis yang tidak perlu diraut dan menghasilkan garis yang halus, pensil mekanik adalah pilihan yang tepat.`
+    },
+    {
+      id: 'bk-bd-BNDR-001',
+      slug: 'buku-binder-bndr-bin',
+      name: "Binder",
+      category: `NEW ITEM`,
+      categorySlug: 'binder',
+      price: 18_000,
+      stock: 30,
+      imageUrl: `/assets/images/notebook.jpeg`,
+      description: `Binder adalah alat yang sangat berguna untuk membantu Anda mengatur dan menyimpan informasi secara efektif. Fleksibilitas dan kepraktisannya membuatnya menjadi pilihan yang populer bagi banyak orang.`
+    },
+    {
+      id: 'spdl-il-LAIN-001',
+      slug: 'spidol-item-lain-il',
+      name: "Spidol",
+      category: `BEST SELLER`,
+      categorySlug: 'item-lain',
+      price: 11_000,
+      stock: 5,
+      imageUrl: `/assets/images/spidol.jpg`,
+      description: `Spidol adalah alat yang sangat berguna untuk membuat tulisan atau gambar sementara di atas permukaan yang halus. Dengan berbagai warna dan jenis yang tersedia, spidol ini dapat membantu Anda dalam berbagai aktivitas, mulai dari presentasi di kantor hingga belajar di sekolah.`
+    },
+    {
+      id: 'mmp-il-LAIN-001',
+      slug: 'memo-pad-item-lain-il',
+      name: "Memo Pad",
+      category: `NEW ITEM`,
+      categorySlug: 'item-lain',
+      price: 9_700,
+      stock: 30,
+      imageUrl: `/assets/images/sticky note.jpg`,
+      description: `Memo pad adalah alat tulis yang sangat berguna untuk membantu kamu tetap terorganisir dan produktif. Dengan ukurannya yang kecil dan fleksibilitasnya yang tinggi, memo pad menjadi salah satu alat tulis yang wajib dimiliki.`
+    },
+    {
+      id: 'cvr-bd-BNDR-001',
+      slug: 'cover-binder-bndr-bin',
+      name: "Cover Binder Warna",
+      category: `NEW ITEM`,
+      categorySlug: 'binder',
+      price: 19_500,
+      stock: 20,
+      imageUrl: `/assets/images/map binder.jpg`,
+      description: `Cover binder berwarna tidak hanya berfungsi sebagai pelindung, tetapi juga sebagai ekspresi diri. Dengan memilih case binder yang tepat, Anda bisa membuat koleksi binder Anda menjadi lebih menarik dan terorganisir.`
+    },
+    {
+      id: 'stplr-il-LAIN-001',
+      slug: 'stapler-item-lain-il',
+      name: "Stapler",
+      category: `BEST BUY`,
+      categorySlug: 'item-lain',
+      price: 18_000,
+      stock: 20,
+      imageUrl: `/assets/images/stapler.png`,
+      description: `Stapler adalah alat yang sangat berguna dalam kehidupan sehari-hari, terutama di kantor atau sekolah. Dengan menggunakan stapler, Anda dapat membuat dokumen menjadi lebih rapi dan profesional.`
+    },
+    {
+      id: 'isi-bd-BNDR-001',
+      slug: 'isi-binder-bndr-bin',
+      name: "Isi Binder",
+      category: `BEST SELLER`,
+      categorySlug: 'binder',
+      price: 11_000,
+      stock: 7,
+      imageUrl: `/assets/images/isi binder.jpeg`,
+      description: `Isi binder adalah komponen penting dari sebuah binder. Dengan memilih isi binder yang tepat, Anda dapat memaksimalkan fungsi binder dan membuat catatan atau dokumen Anda menjadi lebih teratur dan efisien.`
+    },
+    {
+      id: 'cvr-bd-bnng-BNDR-001',
+      slug: 'cover-binder-bening-bndr-bin',
+      name: "Cover Binder Bening",
+      category: `BEST BUY`,
+      categorySlug: 'binder',
+      price: 17_000,
+      stock: 20,
+      imageUrl: `/assets/images/case binder.jpg`,
+      description: `Cover binder bening adalah pilihan yang sangat baik bagi Anda yang menginginkan binder yang praktis, estetik, dan fleksibel. Dengan menggunakan case binder bening, Anda dapat dengan mudah mengorganisir dan menyimpan dokumen-dokumen penting Anda.`
+    },
+    {
+      id: 'mpd-il-LAIN-001',
+      slug: 'map-dokumen-item-lain-il',
+      name: "Map Dokumen",
+      category: `NEW ITEM`,
+      categorySlug: 'item-lain',
+      price: 10_500,
+      stock: 0,
+      imageUrl: `/assets/images/map amplop.jpg`,
+      description: `Map dokumen adalah alat yang sangat berguna untuk mengelola dan menyimpan dokumen. Dengan memilih map yang tepat, Anda dapat menjaga dokumen agar tetap rapi, aman, dan mudah diakses.`
+    },
+    {
+      id: 'klp-bsr-KLIP-001',
+      slug: 'klip-besar-klip-klp',
+      name: "Klip Besar",
+      category: `BEST SELLER`,
+      categorySlug: 'klip',
+      price: 3_000,
+      stock: 0,
+      imageUrl: `/assets/images/klip.jpg`,
+      description: `Klip besar adalah alat yang sangat berguna untuk mengorganisir dan menyimpan dokumen atau benda-benda kecil. Dengan daya jepitnya yang kuat, klip besar dapat membantu Anda menjaga agar dokumen tetap rapi dan tidak mudah tercecer.`
+    },
+    {
+      id: 'klp-kcl-KLIP-001',
+      slug: 'klip-kecil-klip-klp',
+      name: "Klip Kecil",
+      category: `BEST BUY`,
+      categorySlug: 'klip',
+      price: 500,
+      stock: 50,
+      imageUrl: `/assets/images/klip kertas.webp`,
+      description: `Klip kecil adalah alat yang sangat berguna untuk membantu Anda mengatur dan menyimpan dokumen atau benda-benda kecil. Meskipun ukurannya kecil, klip ini memiliki banyak fungsi dan sangat praktis untuk digunakan sehari-hari.`
+    },
+    {
+      id: 'jngk-il-LAIN-001',
+      slug: 'jangka-item-lain-il',
+      name: "Jangka",
+      category: `NEW ITEM`,
+      categorySlug: 'item-lain',
+      price: 20_000,
+      stock: 20,
+      imageUrl: `/assets/images/jangka.jpg`,
+      description: `Jangka adalah alat yang sangat berguna dalam berbagai bidang, terutama dalam kegiatan menggambar dan mengukur. Dengan menggunakan jangka, Anda dapat membuat lingkaran yang sempurna dan akurat.`
+    },
+    {
+      id: 'bk-tls-il-LAIN-001',
+      slug: 'buku-tulis-item-lain-il',
+      name: "Buku Tulis",
+      category: `BEST SELLER`,
+      categorySlug: 'item-lain',
+      price: 6_000,
+      stock: 0,
+      imageUrl: `/assets/images/buku.webp`,
+      description: `Buku tulis adalah alat tulis yang sangat penting dan sering digunakan dalam kehidupan sehari-hari. Dengan menggunakan buku tulis, kita dapat mencatat ide-ide, belajar, dan mengekspresikan diri melalui tulisan atau gambar.`
+    },
+  ]
+}
